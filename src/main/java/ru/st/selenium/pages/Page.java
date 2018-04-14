564b29705f60190eff16e220d408983460e67263
Oleksandra-Shchur <Oleksandra126@gmail.com>
2018-04-14 18:02:32 +0300

    Update Page.java

--- src/main/java/ru/st/selenium/pages/Page.java
+++ src/main/java/ru/st/selenium/pages/Page.java
@@ -2,11 +2,6 @@ package ru.st.selenium.pages;
 
 import org.openqa.selenium.WebDriver;
 
-/*
- * Abstract class representation of a Page in the UI. Page object pattern
- * 
- * @author Sebastiano Armeli-Battana
- */
 public abstract class Page {
 
 	protected WebDriver webDriver;
